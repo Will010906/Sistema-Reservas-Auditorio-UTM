dffbfe8ddb0b85ad2ad863f7d3e7126eb4861a70
Wilmer Ernesto Lobato <alcantarwil@gmail.com>
2026-03-03 16:32:31 -0600

    Modificación a la conexion de la base de datos

--- config/db.php
+++ config/db.php
@@ -1,13 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "reservacionauditorios"; 
+$host = "192.168.99.3";               // IP de la dirección de conexión BD
+$port = "9091";                       // Puerto específico de la base de datos
+$user = "user_equipo11";              // Usuario de la BD
+$password = "user_secret_password11"; // Contraseña de la BD
+$database = "proyecto_equipo11_db";   // Nombre de la base de datos
 
-$conexion = mysqli_connect($host, $user, $password, $database);
+// Conexión incluyendo el puerto
+$conexion = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+    die("Error de conexión al servidor remoto: " . mysqli_connect_error());
 }
-
 ?>
